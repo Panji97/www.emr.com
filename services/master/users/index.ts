182aa2f5a4b4586f371a50c16b0d37e3a5a4ecf8
NasiRames <panjisetyokurniawan@permataazzurisehat.com>
2024-09-24 15:56:46 +0700

    change view user management

--- services/master/users/index.ts
+++ services/master/users/index.ts
@@ -1,35 +1,18 @@
-import { useRef } from 'react'
-import { Toast } from 'primereact/toast'
 import { BASE_MASTER } from '@/ihaaay.json'
 import { getCookie } from '@/helpers/cookies'
 
-export const useUserService = () => {
-    const toast = useRef<Toast>(null)
-    const token = getCookie('access_token')
+const token = getCookie('access_token')
 
-    const getAllUser = async () => {
-        try {
-            const response = await fetch(`${BASE_MASTER}/users`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`
-                }
-            })
-
-            const result = await response.json()
-
-            return result
-        } catch (error: any) {
-            toast.current?.show({
-                severity: 'error',
-                detail: error.message
-            })
+export const getAllUser = async () => {
+    const response = await fetch(`${BASE_MASTER}/users`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
         }
-    }
+    })
 
-    return {
-        toast,
-        getAllUser
-    }
+    const result = await response.json()
+
+    return result
 }
