8e1e0eb7fae95bb01bf2b9a68db1093f08ab80d1
NasiRames <panjisetyokurniawan@permataazzurisehat.com>
2024-11-08 14:09:30 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const accessToken = (request: NextRequest) => {
+export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname
 
     const token = request.cookies.get('access_token')
@@ -9,16 +9,6 @@ const accessToken = (request: NextRequest) => {
     const staticFileRegex = /\.(css|js|jpg|jpeg|png|gif|ico|svg|ttf|woff|woff2)$/
 
     if (staticFileRegex.test(pathname)) return NextResponse.next()
-
-    if (
-        token &&
-        (pathname.startsWith('/auth/login') ||
-            pathname.startsWith('/auth/register') ||
-            pathname.startsWith('/auth/forgot-password') ||
-            pathname.startsWith('/auth/reset-password'))
-    ) {
-        return NextResponse.redirect(new URL('/', request.url))
-    }
 
     if (
         !token &&
@@ -33,53 +23,6 @@ const accessToken = (request: NextRequest) => {
     }
 
     return NextResponse.next()
-}
-
-const accessPermission = (request: NextRequest) => {
-    const menuCookie = request.cookies.get('menu')
-
-    const menu = menuCookie ? JSON.parse(menuCookie.value) : null
-
-    const allowedPaths = menu?.reduce((acc: string[], item: any) => {
-        if (item.items) {
-            item.items.forEach((subItem: any) => acc.push(subItem.to))
-        }
-        return acc
-    }, [])
-
-    let pathname = request.nextUrl.pathname
-
-    const staticFileRegex = /\.(css|js|jpg|jpeg|png|gif|ico|svg|ttf|woff|woff2)$/
-
-    if (staticFileRegex.test(pathname)) return NextResponse.next()
-
-    const validPathRegex = /\/\((main|full-page)\)\//
-
-    if (!validPathRegex.test(pathname)) return NextResponse.next()
-
-    if (pathname.includes('/layout.js')) return NextResponse.next()
-
-    let slicePathname = ''
-    if (pathname.includes('/(main)/')) slicePathname = pathname.slice(31)
-    else if (pathname.includes('/(full-page)/')) slicePathname = pathname.slice(36)
-
-    slicePathname = slicePathname.slice(0, slicePathname.lastIndexOf('/page.js'))
-
-    const isAllowed = allowedPaths?.some((path: any) => path === slicePathname)
-
-    if (!isAllowed) return NextResponse.redirect(new URL('/auth/access', request.url))
-
-    return NextResponse.next()
-}
-
-export function middleware(request: NextRequest) {
-    let response = accessToken(request)
-
-    if (response.status !== 200) return response
-
-    response = accessPermission(request)
-
-    return response
 }
 
 export const config = {
