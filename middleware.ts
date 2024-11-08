4baaed430a5a6b3d4a309f27e18764e7e226a896
NasiRames <panjisetyokurniawan@permataazzurisehat.com>
2024-11-08 09:39:55 +0700

    update type NextRequest

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 const accessToken = (request: NextRequest) => {
     const pathname = request.nextUrl.pathname
