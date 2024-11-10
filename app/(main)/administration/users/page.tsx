60ff695112e17f4504d925a504b33a0c389f204b
NasiRames <panjisetyokurniawan@permataazzurisehat.com>
2024-11-10 17:18:11 +0700

    add redux and adj users page

--- app/(main)/administration/users/page.tsx
+++ app/(main)/administration/users/page.tsx
@@ -1,39 +1,45 @@
 'use client'
-import { Column } from 'primereact/column'
+import { Column, ColumnEditorOptions } from 'primereact/column'
 import { DataTable } from 'primereact/datatable'
-import React from 'react'
+import React, { useState } from 'react'
 import { UserService } from '@/services/master/users'
-import { Button } from 'primereact/button'
+import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown'
 
 const Users = () => {
+    const [roles] = useState<string[]>(['SUPERADMIN', 'USER'])
+
     const { user } = UserService()
+
+    const roleEditor = (options: ColumnEditorOptions) => {
+        console.log(options.value)
+        return (
+            <Dropdown
+                value={options.value}
+                options={roles}
+                onChange={(e: DropdownChangeEvent) => options.editorCallback!(e.value)}
+            />
+        )
+    }
 
     return (
         <div className="grid">
             <div className="col-12">
                 <div className="card">
-                    <DataTable header="User List" value={user}>
+                    <DataTable header="User List" value={user} editMode="row" dataKey="id">
                         <Column field="id" header="Id" />
                         <Column field="email" header="Email" />
                         <Column field="username" header="Username" />
                         <Column field="verify" header="Verified" />
-                        <Column field="user_has_role.role.name" header="Role" />
                         <Column
-                            body={() => (
-                                <>
-                                    <Button
-                                        icon="pi pi-pencil"
-                                        rounded
-                                        raised
-                                        severity="warning"
-                                        className="mr-2"
-                                        onClick={() => {}}
-                                    />
-                                    <Button icon="pi pi-trash" rounded outlined severity="danger" />
-                                </>
-                            )}
+                            field="user_has_role.role.name"
+                            header="Role"
+                            editor={(options) => roleEditor(options)}
                         />
-                        <Column />
+                        <Column
+                            rowEditor={true}
+                            headerStyle={{ width: '10%', minWidth: '8rem' }}
+                            bodyStyle={{ textAlign: 'center' }}
+                        />
                     </DataTable>
                 </div>
             </div>
